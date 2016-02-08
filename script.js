49236e0cd879e31a7940b41e7a876971c8603dc0
John Rachuy II <john.rachuy@gmail.com>
2016-02-08 11:03:39 -0600

    Updated and functional

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+var counter = 0;
+
 $(document).ready(function() {
 	
-	$('#add-button').on('click', function() {
-    	$("input-container").append('<button class="delete">Delete</button>');
-    	$("input-container").append('<button class="change">Change</button>');
+	$('.generate').on('click', function() {
+		counter++;
+		$('.container').append('<div class="wrapper"></div>');
+		var $el = $('.container').children().last();
+		$el.append('<p>' + counter + '</p>');
+		$el.append('<button class="delete">Delete</button>');
+		$el.append('<button class="change">Change</button>');
+	});
 
+	$('.container').on('click', '.delete', function() {
+		$(this).parent().remove();
+	});
+
+	$('.container').on('click', '.change', function() {
+		$(this).parent().toggleClass('highlight');
+	});
 
 });
